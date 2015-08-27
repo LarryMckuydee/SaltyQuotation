ebd5b767349a1c68e99b3163f4d0778d719b208a
LarryMckuydee <larry.mckuydee@gmail.com>
2015-08-27 14:26:39 +0800

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ def shirt_type_fit_relation_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
 
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = ShirtTypeFitRelation.new
     new_record.shirt_type_id = json_hash[i]['shirt_type_id']
     new_record.fit_id = json_hash[i]['fit_id']
@@ -26,7 +26,7 @@ end
 def print_method_block_size_relation_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = PrintMethodBlockSizeRelation.new
     new_record.print_method_id = json_hash[i]['print_method_id']
     new_record.block_size_id = json_hash[i]['block_size_id']
@@ -41,7 +41,7 @@ end
 def shirt_type_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = ShirtType.new
     new_record.name = json_hash[i]['name']
     new_record.save
@@ -51,7 +51,7 @@ end
 def fit_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = Fit.new
     new_record.name = json_hash[i]['name']
     new_record.save
@@ -61,7 +61,7 @@ end
 def brand_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = Brand.new
     new_record.name = json_hash[i]['name']
     new_record.add_on_entitled = json_hash[i]['add_on_entitled']
@@ -72,7 +72,7 @@ end
 def block_size_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = BlockSize.new
     new_record.name = json_hash[i]['name']
     new_record.save
@@ -82,7 +82,7 @@ end
 def print_method_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = PrintMethod.new
     new_record.name = json_hash[i]['name']
     new_record.save
@@ -93,7 +93,7 @@ def relabel_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
 
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = Relabel.new
     new_record.relabel_charge_cents = json_hash[i]['relabel_charge_cents']
     new_record.start_quantity = json_hash[i]['start_quantity']
@@ -106,7 +106,7 @@ def woven_tag_data(file_destination)
   json_file = File.read(file_destination)
   json_hash = JSON.parse(json_file)
 
-  for i in 0..json_hash.count-1
+  for i in (json_hash.count-1).downto(0)
     new_record = WovenTag.new
     new_record.tag_charge_cents = json_hash[i]['tag_charge_cents']
     new_record.start_quantity = json_hash[i]['start_quantity']
@@ -115,13 +115,13 @@ def woven_tag_data(file_destination)
   end
 end
 
-shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations.json'))
-shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations1.json'))
-shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations2.json'))
-shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations3.json'))
 shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations4.json'))
-print_method_block_size_relation_data(Rails.root.join('print_method_block_size_relations.json'))
+shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations3.json'))
+shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations2.json'))
+shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations1.json'))
+shirt_type_fit_relation_data(Rails.root.join('shirt_type_fit_relations.json'))
 print_method_block_size_relation_data(Rails.root.join('print_method_block_size_relations1.json'))
+print_method_block_size_relation_data(Rails.root.join('print_method_block_size_relations.json'))
 shirt_type_data(Rails.root.join('shirt_types.json'))
 block_size_data(Rails.root.join('block_sizes.json'))
 brand_data(Rails.root.join('brands.json'))
@@ -129,3 +129,5 @@ print_method_data(Rails.root.join('print_methods.json'))
 relabel_data(Rails.root.join('relabels.json'))
 woven_tag_data(Rails.root.join('woven_tags.json'))
 fit_data(Rails.root.join('fits.json'))
+
+AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
