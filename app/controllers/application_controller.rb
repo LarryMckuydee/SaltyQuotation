69de0ac5a64687cfb2f3e97c7bbb1c7573c444cb
LarryMckuydee <larry.mckuydee@gmail.com>
2015-08-27 12:12:51 +0800

    Remove http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   USER,PASSWORD = 'happyornot','happy'
   protect_from_forgery with: :exception
-  before_filter :authentication_check
+  # before_filter :authentication_check
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
 
